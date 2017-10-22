c203dce5eaf2231319e85ad71db1e2b644b738c0
unknown <58660138@go.buu.ac.th>
2017-10-22 11:32:44 +0700

    บทนำของ js

---
+++ intro.js
@@ -0,0 +1,49 @@
+var name = 'Ball'
+var count = '0'
+var isCompleted = true
+var price = 10.50
+var a
+var b =null
+
+console.log(typeof name)
+console.log(typeof count)
+console.log(typeof isCompleted)
+console.log(typeof price)
+console.log(typeof a)
+console.log(typeof b)
+
+let a1 = 0
+let isDone = false
+const P1 = 3.142 //ค่าคงที่ไม่เปลี่ยน
+
+if( isCompleted){
+  console.log('Job is completed!');
+}
+
+for (count = 0; count < 5; count++){
+  console.log(count)
+}
+
+while (isCompleted){
+  console.log('Enter while loop')
+  isCompleted = false
+}
+
+var o  ={}
+o.name = 'ball'
+o.email = '58660138@go.buu.ac.th'
+console.log(typeof o)
+console.log(o)
+
+
+var o1 ={
+  name = 'ball'
+  email = '58660138@go.buu.ac.th'
+}
+console.log(o1)
+
+function SimpleObject(){
+
+}
+var so =new SimpleObject()
+console.log(typeof so)
